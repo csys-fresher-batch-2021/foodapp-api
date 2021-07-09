9a81018298843790ac7d4e5241130041472cd063
unknown <rengarajasm@gmail.com>
2021-07-09 17:12:15 +0530

    Added list users mock api

--- server.js
+++ server.js
@@ -7,5 +7,18 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.get('/api/users', getAllUsers);
+
+const users = [
+    {id:1, name:'Admin', email:'admin@gmail.com', password:'admin', role:'ADMIN'},
+    {id:2, name:'Renga', email:'r@gmail.com', password:'user', role:'USER'},
+  ];
+
+function getAllUsers(req,res){
+  res.json(users);  
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
